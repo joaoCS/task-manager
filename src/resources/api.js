c7f6d59992501e38239cc820255589d44ba182c8
joaoCS <joaoantoniogba@hotmail.com>
2023-05-22 15:44:23 -0300

    fix: serverUrl

--- src/resources/api.js
+++ src/resources/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3001"
+    //baseURL: "http://localhost:3001",
+    baseURL: "https://taskmanager456.onrender.com"
 });
 
 export default api;
